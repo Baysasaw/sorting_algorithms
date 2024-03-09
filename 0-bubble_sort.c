1804b4895bb80215b41fea6d52907b268bc19497
yonas <yonas988252656@gmail.com>
2024-03-09 15:51:04 +0300

    Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,25 +1,40 @@
 #include "sort.h"
+
 /**
- * bubble_sort - sort array lements from min to max value
- * @array: array
- * @size: array size
+ * bubble_sort - function that sorts an array of integers in ascending order
+ * using the Bubble sort algorithm
+ *
+ * @array: Array to be sorted
+ * @size: Size of array
  */
-void bubble_sort(int *array, size_t size)
+
+/*
+ * Pseudocode
+ * 1. Loop several times to ensure array is fully sorted
+ * 2.	Loop array from index 0 to second last element
+ * 3.		If element at current index is greater than next element, swap elements
+ */
+void bubble_sort(
+	__attribute__ ((unused)) int *array, /* Pointer */
+	__attribute__ ((unused)) size_t size
+)
 {
+	size_t i, j;
 
-	size_t i, index, tmp = 0;
-
-	if (size < 2)
+	/* Array checks */
+	if (array == NULL || size < 2)
 		return;
+
+
 	for (i = 0; i < size; i++)
-		for (index = 0; index < size; index++)
-		{
-			if (array[index] > array[index + 1] && array[index + 1])
+		for (j = 0; j < size - 1; j++)
+			if (array[j] > array[j + 1])
 			{
-			tmp = array[index];
-			array[index] = array[index + 1];
-			array[index + 1] = tmp;
-			print_array(array, size);
+				int temp = array[j];
+
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+
+				print_array(array, size);
 			}
-		}
 }
